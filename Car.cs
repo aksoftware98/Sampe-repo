94fe111217979175dd4423fe5e2f0df4c50a27c6
Ahmad <aksoftware98@hotmail.com>
2022-03-09 18:41:59 +0400

    Create the required models

---
+++ Car.cs
@@ -0,0 +1,12 @@
+namespace Cars; 
+public class Car 
+{
+    public string? Name {get; set;}
+    public string? Id {get; set;}
+}
+
+public class CarOwner
+{
+    public string? FirstName {get; set;}
+    public string? LastName {get; set;}
+}
